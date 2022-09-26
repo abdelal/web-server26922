91ade5679736898fffad57f221d0bcdfd5add9a6
Ali elal <maxget7@gmail.com>
2022-09-26 15:59:23 +0300

    some cleanups in the db

--- database/db.py
+++ database/db.py
@@ -1,19 +1,15 @@
-from datetime import date
-from functools import reduce
 from sqlite3 import connect
-from sqlite3.dbapi2 import Cursor
-from functools import reduce
+
 DB_NAME = "database/donations.db"  # database name
-# create database inside database folder if not exists
+
 connection = connect(DB_NAME,check_same_thread= False)
 
 
 cursor = connection.cursor()
 def create_table():
     """function to create table inside database"""
-    # create table user inside database if not exists
+    # create table donation inside database if not exists
     table_script = '''CREATE TABLE IF NOT EXISTS Donation(
-
                     amount VARCHAR(255),
                     date VARCHAR(150)
                 );
@@ -28,7 +24,6 @@ def get_summery():
     
     total = sum([float(x[0]) for x in data])
 
-    print(len(data))
     return total,len(data)
 
 def insert_donation(amount, date):
@@ -39,6 +34,6 @@ def insert_donation(amount, date):
 
 
 def fetch_records():
-    """function to fetch User records"""
+    """function to fetch donation records"""
     data = cursor.execute("SELECT * FROM Donation")
     return data
